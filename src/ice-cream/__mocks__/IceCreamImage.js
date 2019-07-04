db1aa1f79962939afdd7e76abe01f75a179537ff
AlmeroSteyn <almero.steyn@gmail.com>
2019-07-04 13:42:34 +0200

    Remove tests

--- src/ice-cream/__mocks__/IceCreamImage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const IceCreamImage = ({ iceCreamId }) => (
-  <img src={`ice-cream-${iceCreamId}.svg`} alt="" />
-);
-
-export default IceCreamImage;
--- src/structure/__mocks__/FocusLink.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const FocusLink = ({ to, children, ...props }) => (
-  <a href={to} {...props}>
-    {children}
-  </a>
-);
-
-export default FocusLink;
