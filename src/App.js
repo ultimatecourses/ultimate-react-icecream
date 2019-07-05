8429b3b904927b7744b7ca95a8d183c94abfea60
AlmeroSteyn <almero.steyn@gmail.com>
2019-07-05 11:12:06 +0200

    Removed content of 0 to 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './structure/Header';
-import Footer from './structure/Footer';
-import Menu from './ice-cream/Menu';
 import './styles/ice-cream.scss';
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Route path="/" component={Menu} exact />
-      <Footer />
-    </Router>
+    <header>
+      <h1>Your app goes here</h1>
+    </header>
   );
 };
 
--- src/structure/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer>
-    <span>&copy; Ultimate Ice Cream</span>
-  </footer>
-);
-
-export default Footer;
