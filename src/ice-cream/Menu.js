002b08d6081ed738cd0cd5205afc5fafa7f8a118
AlmeroSteyn <almero.steyn@gmail.com>
2019-07-31 14:37:29 +0200

    Style empty list paragraph and simplify JSX logic

--- src/ice-cream/Menu.js
+++ src/ice-cream/Menu.js
@@ -35,38 +35,34 @@ const Menu = () => {
         doneMsg="Loading menu complete."
         isLoading={isLoading}
       />
-      {!isLoading && (
-        <div>
-          {menu.length > 0 && !isLoading ? (
-            <ul className="container">
-              {menu.map(
-                ({ id, iceCream, price, description, inStock, quantity }) => (
-                  <li key={id.toString()}>
-                    <section className="card">
-                      <div className="image-container">
-                        <IceCreamImage iceCreamId={iceCream.id} />
-                      </div>
-                      <div className="text-container">
-                        <h3>{iceCream.name}</h3>
-                        <div className="content card-content">
-                          <p className="price">{`$${price.toFixed(2)}`}</p>
-                          <p className={`stock${inStock ? '' : ' out'}`}>
-                            {inStock
-                              ? `${quantity} in stock`
-                              : 'Currently out of stock!'}
-                          </p>
-                          <p className="description">{description}</p>
-                        </div>
-                      </div>
-                    </section>
-                  </li>
-                )
-              )}
-            </ul>
-          ) : (
-            <p>Your menu is empty! The sadness!!</p>
+      {menu.length > 0 ? (
+        <ul className="container">
+          {menu.map(
+            ({ id, iceCream, price, description, inStock, quantity }) => (
+              <li key={id.toString()}>
+                <section className="card">
+                  <div className="image-container">
+                    <IceCreamImage iceCreamId={iceCream.id} />
+                  </div>
+                  <div className="text-container">
+                    <h3>{iceCream.name}</h3>
+                    <div className="content card-content">
+                      <p className="price">{`$${price.toFixed(2)}`}</p>
+                      <p className={`stock${inStock ? '' : ' out'}`}>
+                        {inStock
+                          ? `${quantity} in stock`
+                          : 'Currently out of stock!'}
+                      </p>
+                      <p className="description">{description}</p>
+                    </div>
+                  </div>
+                </section>
+              </li>
+            )
           )}
-        </div>
+        </ul>
+      ) : (
+        !isLoading && <p>Your menu is empty! The sadness!!</p>
       )}
     </main>
   );
