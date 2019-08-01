d89d17d3e0b11b3e1e685851fdf9efa45bff900e
AlmeroSteyn <almero.steyn@gmail.com>
2019-08-01 15:59:33 +0200

    Small bugfix

--- src/ice-cream/EditIceCream.js
+++ src/ice-cream/EditIceCream.js
@@ -15,7 +15,7 @@ import {
 
 const EditIceCream = ({ match, history }) => {
   const isMounted = useRef(true);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [menuItem, setMenuItem] = useState({
     price: '0.00',
     inStock: true,
@@ -69,6 +69,7 @@ const EditIceCream = ({ match, history }) => {
   }, []);
 
   useEffect(() => {
+    setIsLoading(true);
     getMenuItem(match.params.menuItemId)
       .then(({ id, price, inStock, quantity, description, iceCream }) => {
         if (isMounted.current) {
